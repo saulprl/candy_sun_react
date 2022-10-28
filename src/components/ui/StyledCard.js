a25ae714363e2c3670bba4f6ebfddd03060ee03f
Saul <phasecross@hotmail.com>
2022-10-28 14:11:34 -0700

    Set responsive padding and borderRadius

--- src/components/ui/StyledCard.js
+++ src/components/ui/StyledCard.js
@@ -5,8 +5,8 @@ const StyledCard = (props) => {
     <Card
       variant="outlined"
       sx={{
-        padding: "1rem",
-        borderRadius: "14px",
+        padding: { xs: "0", sm: "1rem" },
+        borderRadius: { xs: "6px", sm: "12px" },
         boxShadow: "0 1px 18px 10px rgba(0, 0, 0, 0.24)",
         ...props.sx,
       }}
