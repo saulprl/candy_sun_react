8140270e2326ccaa580042d518a8a16a7bf625ad
Saul <phasecross@hotmail.com>
2022-10-28 14:06:26 -0700

    Added firebaseConfig object from Firebase Console

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXN2KGE8DoWKahINLBCy5VIwfIm1I0nUk",
+  authDomain: "candy-sun.firebaseapp.com",
+  databaseURL: "https://candy-sun-default-rtdb.firebaseio.com",
+  projectId: "candy-sun",
+  storageBucket: "candy-sun.appspot.com",
+  messagingSenderId: "335784904935",
+  appId: "1:335784904935:web:87f8cc485a9b43d7a3fa42",
+  measurementId: "G-71FTCM8672",
+};
+
+export default firebaseConfig;
