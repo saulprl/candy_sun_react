c96159e79ac99eab8df832f96f26e75ed57b9fbd
Saul <phasecross@hotmail.com>
2022-10-28 14:09:41 -0700

    Set up data fetch from Firestore with conditional content

--- src/components/products/AvailableProducts.js
+++ src/components/products/AvailableProducts.js
@@ -1,7 +1,60 @@
 // import { useState, useEffect, useCallback } from "react";
+import { CardContent, List, Skeleton, Stack } from "@mui/material";
+import { collection, query } from "firebase/firestore";
+import { useFirestore, useFirestoreCollectionData } from "reactfire";
+import StyledCard from "../ui/StyledCard";
+import ProductItem from "./product-item/ProductItem";
 
 const AvailableProducts = (props) => {
-  
+  const productsCollection = collection(useFirestore(), "products");
+  // console.log(productsRef);
+  const productsQuery = query(productsCollection);
+  const { status, data } = useFirestoreCollectionData(productsQuery, {
+    idField: "id",
+  });
+  let content;
+  if (status === "loading") {
+    content = (
+      <Stack spacing={1.5}>
+        <Skeleton
+          animation="wave"
+          variant="rounded"
+          sx={{ width: "100%" }}
+          height={60}
+        />
+        <Skeleton
+          animation="wave"
+          variant="rounded"
+          sx={{ width: "100%" }}
+          height={60}
+        />
+        <Skeleton
+          animation="wave"
+          variant="rounded"
+          sx={{ width: "100%" }}
+          height={60}
+        />
+        <Skeleton animation="wave" variant="text" sx={{ fontSize: "1rem" }} />
+      </Stack>
+    );
+  } else if (status === "success") {
+    content = data.map((p) => <ProductItem key={p.id} product={p} />);
+  }
+
+  return (
+    <StyledCard
+      sx={{
+        width: { xs: "90%", lg: "70%" },
+        mt: "1rem",
+        mr: "auto",
+        ml: "auto",
+      }}
+    >
+      <CardContent>
+        <List>{content}</List>
+      </CardContent>
+    </StyledCard>
+  );
 };
 
 export default AvailableProducts;
