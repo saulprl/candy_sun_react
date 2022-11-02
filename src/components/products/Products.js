ce4bee99fdf7464c92b43759874526672c16eade
Saul <phasecross@hotmail.com>
2022-11-02 13:40:28 -0700

    Added sign in status handling with alerts

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,9 +1,13 @@
-import { Box } from "@mui/material";
+import { useSigninCheck } from "reactfire";
+
+import { Alert, Box } from "@mui/material";
+
 import AvailableProducts from "./AvailableProducts";
 import ProductsSummary from "./ProductsSummary";
 
 const Products = (props) => {
   const drawerWidth = props.drawerWidth;
+  const { status, data: signInCheckResult } = useSigninCheck();
 
   return (
     <Box
@@ -13,7 +17,23 @@ const Products = (props) => {
       }}
     >
       <ProductsSummary />
-      <AvailableProducts />
+      {status === "success" && !signInCheckResult.signedIn && (
+        <Box
+          sx={{ margin: "auto", mt: "1.2rem", width: { xs: "90%", lg: "65%" } }}
+        >
+          <Alert severity="error">You are not authenticated!</Alert>
+        </Box>
+      )}
+      {status === "loading" && (
+        <Box
+          sx={{ margin: "auto", mt: "1.2rem", width: { xs: "90%", lg: "65%" } }}
+        >
+          <Alert severity="info">Authenticating...</Alert>
+        </Box>
+      )}
+      {status === "success" && signInCheckResult.signedIn && (
+        <AvailableProducts />
+      )}
     </Box>
   );
 };
