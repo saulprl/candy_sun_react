e4b4c90ddaebe23fc9cf0bb64bbf1a484d13f7c2
Saul <phasecross@hotmail.com>
2022-11-02 13:38:39 -0700

    Added all the login logic

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,7 @@
-import { forwardRef, useRef } from "react";
+import { useState } from "react";
+import { useFirebaseApp, useSigninCheck } from "reactfire";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 import {
   Box,
   CardContent,
@@ -7,86 +10,125 @@ import {
   Typography,
   TextField,
   Button,
+  Alert,
+  Collapse,
 } from "@mui/material";
 
 import StyledCard from "../ui/StyledCard";
+import LoadingButton from "../ui/LoadingButton";
 
 const Login = (props) => {
-  const modalRoot = document.body;
-  const email = useRef();
-  const password = useRef();
+  const firebaseApp = useFirebaseApp();
+  const authInstance = getAuth(firebaseApp);
+  const { status, data: signinCheckResult } = useSigninCheck();
 
-  const submitHandler = (event) => {
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const modalRoot = document.body;
+
+  const emailChangeHandler = (event) => {
+    setError("");
+    setEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setError("");
+    setPassword(event.target.value);
+  };
+
+  const submitHandler = async (event) => {
     event.preventDefault();
-    props.onLogin({
-      email,
-      password,
-    });
+    // console.log({ email, password });
+    try {
+      await signInWithEmailAndPassword(authInstance, email, password);
+      // props.onClose(event);
+      window.location.reload(false);
+    } catch (error) {
+      if (error.code === "auth/user-not-found") {
+        setError("Authentication error: user not found.");
+      }
+      if (error.code === "auth/wrong-password") {
+        setError("Authentication error: incorrect password.");
+      }
+    }
   };
 
   return (
     <Portal container={modalRoot}>
       <Modal open={props.open} onClose={props.onClose}>
-        <StyledCard
-          sx={{
-            width: { xs: "70%", lg: "40%" },
-            margin: "auto",
-            mt: { xs: "40%", sm: "20%" },
-          }}
-        >
-          <CardContent>
-            <Typography variant="h5">Login</Typography>
-            <Box sx={{ height: "0.8rem" }} />
-            <form onSubmit={submitHandler}>
-              <Box
-                component="div"
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignContent: "flex-start",
-                  justifyContent: "space-between",
-                }}
-              >
-                <TextField
-                  inputRef={email}
-                  id="email"
-                  label="Email"
-                  type="email"
-                  required
-                />
-                <Box sx={{ height: "1.2rem" }} />
-                <TextField
-                  inputRef={password}
-                  id="password"
-                  label="Password"
-                  type="password"
-                  required
-                />
-              </Box>
-              <Box sx={{ height: "1.2rem" }} />
-              <Box
-                component="div"
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "right",
-                }}
-              >
-                <Button variant="contained" color="primary" type="submit">
-                  Login
-                </Button>
-                <Box sx={{ width: "0.5rem" }} />
-                <Button
-                  variant="contained"
-                  color="error"
-                  onClick={props.onClose}
+        <Box>
+          <StyledCard
+            sx={{
+              width: { xs: "70%", lg: "40%" },
+              margin: "auto",
+              mt: { xs: "40%", sm: "20%" },
+            }}
+          >
+            <CardContent>
+              <Typography variant="h5">Login</Typography>
+              <Box sx={{ height: "0.8rem" }} />
+              <form onSubmit={submitHandler}>
+                <Box
+                  component="div"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignContent: "flex-start",
+                    justifyContent: "space-between",
+                  }}
                 >
-                  Cancel
-                </Button>
-              </Box>
-            </form>
-          </CardContent>
-        </StyledCard>
+                  <TextField
+                    onChange={emailChangeHandler}
+                    id="email"
+                    label="Email"
+                    type="email"
+                    required
+                  />
+                  <Box sx={{ height: "1.2rem" }} />
+                  <TextField
+                    onChange={passwordChangeHandler}
+                    id="password"
+                    label="Password"
+                    type="password"
+                    required
+                  />
+                </Box>
+                <Box sx={{ mt: "0.8rem" }}>
+                  <Collapse in={error.trim() !== ""}>
+                    <Alert severity="error">{error}</Alert>
+                  </Collapse>
+                </Box>
+                <Box sx={{ height: "1.2rem" }} />
+                <Box
+                  component="div"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "right",
+                  }}
+                >
+                  <LoadingButton
+                    variant="contained"
+                    type="submit"
+                    color="primary"
+                    status={status}
+                  >
+                    Login
+                  </LoadingButton>
+                  <Box sx={{ width: "0.5rem" }} />
+                  <Button
+                    variant="contained"
+                    color="error"
+                    onClick={props.onClose}
+                  >
+                    Cancel
+                  </Button>
+                </Box>
+              </form>
+            </CardContent>
+          </StyledCard>
+        </Box>
       </Modal>
     </Portal>
   );
