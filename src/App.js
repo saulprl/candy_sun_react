ccf3ee63f5d7d886485b641653ca1cfe20b58074
Saul <phasecross@hotmail.com>
2022-11-02 13:37:02 -0700

    Removed login logic and added an error boundary

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React, { useState } from "react";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { AuthProvider, FirestoreProvider, useFirebaseApp } from "reactfire";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
 
 import Login from "./components/auth/Login";
 import Header from "./components/layout/Header";
 import Products from "./components/products/Products";
+import ErrorBoundary from "./components/error/ErrorBoundary";
 
 import styles from "./App.module.css";
 
 const App = () => {
   const drawerWidth = 240;
-  const [showLogin, setShowLogin] = useState(false);
   const [darkMode, setDarkMode] = useState(true);
+  const [showLogin, setShowLogin] = useState(false);
 
   const firebaseApp = useFirebaseApp();
-  const firestoreInstance = getFirestore(firebaseApp);
   const authInstance = getAuth(firebaseApp);
+  const firestoreInstance = getFirestore(firebaseApp);
 
   const theme = createTheme({
     palette: {
@@ -65,38 +66,35 @@ const App = () => {
     setShowLogin(false);
   };
 
-  const loginHandler = async (userCredentials) => {
-    const user = await signInWithEmailAndPassword(
-      authInstance,
-      userCredentials.email,
-      userCredentials.password
-    );
-    console.log(user.user);
+  const logoutHandler = async (event) => {
+    signOut(authInstance);
   };
 
   return (
     // <div className={styles["App-header"]}>
-    <AuthProvider sdk={authInstance}>
-      <FirestoreProvider sdk={firestoreInstance}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {showLogin && (
-            <Login
-              open={showLogin}
+    <ErrorBoundary>
+      <AuthProvider sdk={authInstance}>
+        <FirestoreProvider sdk={firestoreInstance}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {showLogin && (
+              <Login
+                open={showLogin}
+                drawerWidth={drawerWidth}
+                onClose={closeLoginHandler}
+              />
+            )}
+            <Header
               drawerWidth={drawerWidth}
-              onClose={closeLoginHandler}
-              onLogin={loginHandler}
+              onLogout={logoutHandler}
+              onShowLogin={showLoginHandler}
+              onToggleTheme={toggleThemeHandler}
             />
-          )}
-          <Header
-            drawerWidth={drawerWidth}
-            onShowLogin={showLoginHandler}
-            onToggleTheme={toggleThemeHandler}
-          />
-          <Products drawerWidth={drawerWidth} />
-        </ThemeProvider>
-      </FirestoreProvider>
-    </AuthProvider>
+            <Products drawerWidth={drawerWidth} />
+          </ThemeProvider>
+        </FirestoreProvider>
+      </AuthProvider>
+    </ErrorBoundary>
     // </div>
   );
 };
