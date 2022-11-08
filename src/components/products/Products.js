c835551681f0157aab541e3ca968d081b7e756a4
Saul <phasecross@hotmail.com>
2022-11-08 13:38:09 -0700

    Formatted and added conditional rendering (SplashScreen, Redirect, or Products)

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -4,37 +4,55 @@ import { Alert, Box } from "@mui/material";
 
 import AvailableProducts from "./AvailableProducts";
 import ProductsSummary from "./ProductsSummary";
+import SplashScreen from "../ui/SplashScreen";
+import { Redirect } from "react-router-dom";
 
 const Products = (props) => {
   const drawerWidth = props.drawerWidth;
   const { status, data: signInCheckResult } = useSigninCheck();
 
   return (
-    <Box
-      sx={{
-        width: { sm: `calc(100% - ${drawerWidth}px)` },
-        ml: { sm: `${drawerWidth}px` },
-      }}
-    >
-      <ProductsSummary />
+    <>
+      {status === "loading" && <SplashScreen />}
       {status === "success" && !signInCheckResult.signedIn && (
-        <Box
-          sx={{ margin: "auto", mt: "1.2rem", width: { xs: "90%", lg: "65%" } }}
-        >
-          <Alert severity="error">You are not authenticated!</Alert>
-        </Box>
-      )}
-      {status === "loading" && (
-        <Box
-          sx={{ margin: "auto", mt: "1.2rem", width: { xs: "90%", lg: "65%" } }}
-        >
-          <Alert severity="info">Authenticating...</Alert>
-        </Box>
+        <Redirect to="/login" />
       )}
       {status === "success" && signInCheckResult.signedIn && (
-        <AvailableProducts />
+        <Box
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+          }}
+        >
+          <ProductsSummary />
+          {status === "success" && !signInCheckResult.signedIn && (
+            <Box
+              sx={{
+                margin: "auto",
+                mt: "1.2rem",
+                width: { xs: "90%", lg: "65%" },
+              }}
+            >
+              <Alert severity="error">You are not authenticated!</Alert>
+            </Box>
+          )}
+          {status === "loading" && (
+            <Box
+              sx={{
+                margin: "auto",
+                mt: "1.2rem",
+                width: { xs: "90%", lg: "65%" },
+              }}
+            >
+              <Alert severity="info">Authenticating...</Alert>
+            </Box>
+          )}
+          {status === "success" && signInCheckResult.signedIn && (
+            <AvailableProducts />
+          )}
+        </Box>
       )}
-    </Box>
+    </>
   );
 };
 
