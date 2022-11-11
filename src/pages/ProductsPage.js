e9c1491fd9fb6d40e299445b4bcdd1ef18cf5c15
Saul <phasecross@hotmail.com>
2022-11-11 14:03:27 -0700

    Added /products/add exact route

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,6 +1,7 @@
-import { Redirect } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import { useSigninCheck } from "reactfire";
 import Products from "../components/products/Products";
+import ProductsForm from "../components/products/ProductsForm";
 import SplashScreen from "../components/ui/SplashScreen";
 
 const ProductsPage = (props) => {
@@ -8,13 +9,18 @@ const ProductsPage = (props) => {
 
   return (
     <>
-      {status === "loading" && <SplashScreen />}
-      {status === "success" && !signInCheckResult.signedIn && (
-        <Redirect to="/login" />
-      )}
-      {status === "success" && signInCheckResult.signedIn && (
-        <Products drawerWidth={props.drawerWidth} />
-      )}
+      <Route path="/products" exact>
+        {status === "loading" && <SplashScreen />}
+        {status === "success" && !signInCheckResult.signedIn && (
+          <Redirect to="/login" />
+        )}
+        {status === "success" && signInCheckResult.signedIn && (
+          <Products drawerWidth={props.drawerWidth} />
+        )}
+      </Route>
+      <Route path="/products/add" exact>
+        <ProductsForm />
+      </Route>
     </>
   );
 };
