98817882c11d4b058ef0596f45d93042b17506b1
Saul <phasecross@hotmail.com>
2022-11-11 16:07:31 -0700

    Added a component to display whenever there's an active notification

---
+++ src/components/ui/Notification.js
@@ -0,0 +1,43 @@
+import { useSelector } from "react-redux";
+
+import { selectNotification } from "../../store/uiSlice";
+
+import { Alert, Box, CircularProgress, Slide } from "@mui/material";
+
+const Notification = (props) => {
+  const notification = useSelector(selectNotification);
+
+  return (
+    <Slide
+      direction="down"
+      in={notification !== null}
+      mountOnEnter
+      unmountOnExit
+    >
+      <Box
+        sx={{
+          zIndex: "999",
+          position: "absolute",
+          width: { xs: "90%", sm: "75%" },
+          mt: "3.5rem",
+          ml: { xs: "5%", sm: "12.5%" },
+        }}
+      >
+        {notification === null && (
+          <Alert severity="info">
+            <Box sx={{ width: "100%" }}>
+              <CircularProgress color="primary" size="1rem" />
+            </Box>
+          </Alert>
+        )}
+        {notification !== null && (
+          <Alert severity={notification.status || "info"}>
+            {notification.message || <CircularProgress color="primary" />}
+          </Alert>
+        )}
+      </Box>
+    </Slide>
+  );
+};
+
+export default Notification;
