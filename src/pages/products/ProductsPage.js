053550ce5f5f7b3a90d3574d8ca8aa606a43ee03
Saul <phasecross@hotmail.com>
2022-11-12 13:52:47 -0700

    Removed signInCheck occurrences, conditional rendering, and optimized Routes with render prop

--- src/pages/products/ProductsPage.js
+++ src/pages/products/ProductsPage.js
@@ -1,42 +1,35 @@
-import { Redirect, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
-import { useSigninCheck } from "reactfire";
+import { Route, useLocation } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { setTitle } from "../../store/uiSlice";
 
 import AddProductPage from "./AddProductPage";
 import EditProductPage from "./EditProductPage";
 
 import Products from "../../components/products/Products";
-import SplashScreen from "../../components/ui/SplashScreen";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setTitle } from "../../store/uiSlice";
 
 const ProductsPage = (props) => {
   const dispatch = useDispatch();
   const location = useLocation();
-  const { status, data: signInCheckResult } = useSigninCheck();
 
   useEffect(() => {
     dispatch(setTitle("Productos"));
-  }, [status, dispatch, location.pathname]);
+  }, [dispatch, location.pathname]);
 
   return (
     <>
-      <Route path="/products" exact>
-        {status === "loading" && <SplashScreen />}
-        {status === "success" && !signInCheckResult.signedIn && (
-          <Redirect to="/login" />
-        )}
-        {status === "success" && signInCheckResult.signedIn && (
-          <Products drawerWidth={props.drawerWidth} />
-        )}
-      </Route>
-      <Route path="/products/add" exact>
-        <AddProductPage />
-      </Route>
-      <Route path="/products/edit/:productId">
-        <EditProductPage />
-      </Route>
+      <Route
+        path="/products"
+        exact
+        render={() => <Products drawerWidth={props.drawerWidth} />}
+      />
+      <Route path="/products/add" exact render={() => <AddProductPage />} />
+      <Route
+        path="/products/edit/:productId"
+        render={() => <EditProductPage />}
+      />
     </>
   );
 };
