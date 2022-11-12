6c376f18ef959970a7ce48c9e5eeca266ef4baa3
Saul <phasecross@hotmail.com>
2022-11-12 12:01:20 -0700

    Now dispatches a new header title

--- src/pages/products/AddProductPage.js
+++ src/pages/products/AddProductPage.js
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import ProductsForm from "../../components/products/ProductsForm";
+import { setTitle } from "../../store/uiSlice";
 
 const AddProductPage = (props) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setTitle("Agregar producto"));
+  }, [dispatch]);
+
   return <ProductsForm />;
 };
 
