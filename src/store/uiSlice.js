aa8db42ebd7cb24686caaae0be925980bb88afea
Saul <phasecross@hotmail.com>
2022-11-12 13:53:15 -0700

    Added themeMode state and toggle actions

--- src/store/uiSlice.js
+++ src/store/uiSlice.js
@@ -5,10 +5,14 @@ const uiSlice = createSlice({
   initialState: {
     headerTitle: "Inicio",
     notification: null,
+    themeMode: "dark",
   },
   reducers: {
     setTitle: (state, action) => {
       state.headerTitle = action.payload;
+    },
+    toggleTheme: (state) => {
+      state.themeMode = state.themeMode === "dark" ? "light" : "dark";
     },
     showNotification: (state, action) => {
       state.notification = {
@@ -30,10 +34,11 @@ export const ephimeralNotification = (notificationData) => (dispatch) => {
   dispatch(showNotification(notificationData));
 };
 
-export const { setTitle, showNotification, clearNotification } =
+export const { setTitle, toggleTheme, showNotification, clearNotification } =
   uiSlice.actions;
 
 export const selectHeaderTitle = (state) => state.ui.headerTitle;
 export const selectNotification = (state) => state.ui.notification;
+export const selectThemeMode = (state) => state.ui.themeMode;
 
 export default uiSlice.reducer;
