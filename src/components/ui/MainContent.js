261b0e6fb3d74f88ba73e25b72c555e3fc0b32a5
Saul <phasecross@hotmail.com>
2022-11-12 13:51:33 -0700

    Removed signInCheck occurrences and conditional rendering

--- src/components/ui/MainContent.js
+++ src/components/ui/MainContent.js
@@ -1,31 +1,15 @@
-import { useSigninCheck } from "reactfire";
-
-import { Redirect } from "react-router-dom";
-
 import { Box } from "@mui/material";
 
-import SplashScreen from "./SplashScreen";
-
 const MainContent = (props) => {
-  const { status, data: signInCheckResult } = useSigninCheck();
-
   return (
-    <>
-      {status === "loading" && <SplashScreen />}
-      {status === "success" && !signInCheckResult.signedIn && (
-        <Redirect to="/login" />
-      )}
-      {status === "success" && signInCheckResult.signedIn && (
-        <Box
-          sx={{
-            width: { sm: `calc(100% - ${props.drawerWidth})` },
-            ml: { sm: `${props.drawerWidth}px` },
-          }}
-        >
-          {props.children}
-        </Box>
-      )}
-    </>
+    <Box
+      sx={{
+        width: { sm: `calc(100% - ${props.drawerWidth})` },
+        ml: { sm: `${props.drawerWidth}px` },
+      }}
+    >
+      {props.children}
+    </Box>
   );
 };
 
