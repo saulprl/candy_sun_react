70125de23935f3af46b42639d9b3f81149c194b4
Saul <phasecross@hotmail.com>
2022-11-12 15:39:25 -0700

    Renamed from AvailableProducts

---
+++ src/components/products/ProductsList.js
@@ -0,0 +1,130 @@
+import { collection, query } from "firebase/firestore";
+import { useFirestore, useFirestoreCollectionData } from "reactfire";
+
+import {
+  CardContent,
+  Grid,
+  Skeleton,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+
+import StyledCard from "../ui/StyledCard";
+import ProductItem from "./product-item/ProductItem";
+
+const ProductsList = (props) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const { searchFilter } = props;
+  const productsCollection = collection(useFirestore(), "products");
+  const productsQuery = query(productsCollection);
+  const { status, data } = useFirestoreCollectionData(productsQuery, {
+    idField: "id",
+  });
+
+  if (status === "loading") {
+    return (
+      <StyledCard
+        sx={{
+          width: { xs: "90%", lg: "70%" },
+          position: "relative",
+          margin: "auto",
+          mt: "1rem",
+          mb: "1rem",
+        }}
+      >
+        <CardContent>
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Skeleton
+                animation="wave"
+                variant="rounded"
+                height={60}
+                sx={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Skeleton
+                animation="wave"
+                variant="rounded"
+                height={60}
+                sx={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Skeleton
+                animation="wave"
+                variant="rounded"
+                height={60}
+                sx={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Skeleton
+                animation="wave"
+                variant="rounded"
+                height={60}
+                sx={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Skeleton
+                animation="wave"
+                variant="rounded"
+                height={60}
+                sx={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Skeleton
+                animation="wave"
+                variant="rounded"
+                height={60}
+                sx={{ width: "100%" }}
+              />
+            </Grid>
+          </Grid>
+        </CardContent>
+      </StyledCard>
+    );
+  }
+
+  if (status === "success") {
+    const content = data
+      .filter((p) => p.title.toLowerCase().includes(searchFilter.toLowerCase()))
+      .map((p) => (
+        <Grid item key={p.id} xs={12} sm={6} md={4}>
+          <ProductItem product={p} />
+        </Grid>
+      ));
+
+    return (
+      <StyledCard
+        sx={{
+          width: { xs: "90%", lg: "70%" },
+          position: "relative",
+          mt: "1rem",
+          mr: "auto",
+          ml: "auto",
+          mb: "1rem",
+        }}
+      >
+        {isMobile && (
+          <CardContent>
+            <Grid container spacing={1}>
+              {content}
+            </Grid>
+          </CardContent>
+        )}
+        {!isMobile && (
+          <Grid container spacing={1}>
+            {content}
+          </Grid>
+        )}
+      </StyledCard>
+    );
+  }
+};
+
+export default ProductsList;
