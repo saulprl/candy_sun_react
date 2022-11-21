9b83014f8230ecee05cae0a7b7477a4a2edfcfd9
Saul <phasecross@hotmail.com>
2022-11-21 09:21:52 -0700

    Notification is now portaled into div#notification and position is now fixed

--- src/components/ui/Notification.js
+++ src/components/ui/Notification.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 
-import { Alert, Box, CircularProgress, Slide } from "@mui/material";
+import { Alert, Box, CircularProgress, Portal, Slide } from "@mui/material";
 
 import { selectNotification } from "../../store/uiSlice";
 
@@ -9,43 +9,45 @@ const Notification = (props) => {
   const notification = useSelector(selectNotification);
 
   return (
-    <Slide
-      direction="down"
-      in={notification !== null}
-      mountOnEnter
-      unmountOnExit
-    >
-      <Box
-        sx={{
-          zIndex: "999",
-          position: "absolute",
-          width: {
-            xs: "90%",
-            sm: `calc(calc(100% - ${drawerWidth}px) * 0.75)`,
-            md: `calc(calc(100% - ${drawerWidth}px) * 0.6)`,
-          },
-          mt: "3.5rem",
-          ml: {
-            xs: "5%",
-            sm: `calc(calc(100% - ${drawerWidth}px) * 0.125)`,
-            md: `calc(calc(100% - ${drawerWidth}px) * 0.2)`,
-          },
-        }}
+    <Portal container={document.getElementById("notification")}>
+      <Slide
+        direction="down"
+        in={notification !== null}
+        mountOnEnter
+        unmountOnExit
       >
-        {notification === null && (
-          <Alert severity="info">
-            <Box sx={{ width: "100%" }}>
-              <CircularProgress color="primary" size="1rem" />
-            </Box>
-          </Alert>
-        )}
-        {notification !== null && (
-          <Alert severity={notification.status || "info"}>
-            {notification.message || <CircularProgress color="primary" />}
-          </Alert>
-        )}
-      </Box>
-    </Slide>
+        <Box
+          sx={{
+            zIndex: "999",
+            position: "fixed",
+            width: {
+              xs: "90%",
+              sm: `calc(calc(100% - ${drawerWidth}px) * 0.75)`,
+              md: `calc(calc(100% - ${drawerWidth}px) * 0.6)`,
+            },
+            mt: "3.5rem",
+            ml: {
+              xs: "5%",
+              sm: `calc(calc(100% - ${drawerWidth}px) * 0.125)`,
+              md: `calc(calc(100% - ${drawerWidth}px) * 0.2)`,
+            },
+          }}
+        >
+          {notification === null && (
+            <Alert severity="info">
+              <Box sx={{ width: "100%" }}>
+                <CircularProgress color="primary" size="1rem" />
+              </Box>
+            </Alert>
+          )}
+          {notification !== null && (
+            <Alert severity={notification.status || "info"}>
+              {notification.message || <CircularProgress color="primary" />}
+            </Alert>
+          )}
+        </Box>
+      </Slide>
+    </Portal>
   );
 };
 
