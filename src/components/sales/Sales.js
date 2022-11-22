5a3e44f1cbef97b29892521afa2e5525bc1c18bb
Saul <phasecross@hotmail.com>
2022-11-21 20:09:18 -0700

    Removed eveything related to the search bar

--- src/components/sales/Sales.js
+++ src/components/sales/Sales.js
@@ -1,53 +1,14 @@
-import { useEffect, useRef, useState } from "react";
-
 import { useHistory } from "react-router-dom";
 
-import { Input } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import SearchIcon from "@mui/icons-material/Search";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import ActionBar from "../ui/ActionBar";
 import SalesList from "./SalesList";
 
-const Sales = (props) => {
+const Sales = () => {
   const history = useHistory();
-  const searchInputRef = useRef();
-
-  const [showSearchBar, setShowSearchBar] = useState(false);
-  const [filter, setFilter] = useState("");
-
-  useEffect(() => {
-    if (showSearchBar) {
-      searchInputRef.current.focus();
-    }
-  }, [showSearchBar]);
-
-  const searchActionHandler = (event) => {
-    setShowSearchBar((prevState) => !prevState);
-    setFilter("");
-  };
-
-  const searchChangeHandler = (event) => {
-    setFilter(event.target.value);
-  };
 
   const actions = [
-    {
-      label: "Buscar productos",
-      color: "info",
-      icon: <SearchIcon />,
-      onClick: searchActionHandler,
-      hiddenElement: (
-        <Input
-          inputRef={searchInputRef}
-          color="info"
-          placeholder="Buscar productos"
-          onChange={searchChangeHandler}
-          value={filter}
-          sx={{ maxWidth: "90%" }}
-        />
-      ),
-    },
     {
       label: "Registrar venta",
       color: "primary",
@@ -64,8 +25,8 @@ const Sales = (props) => {
 
   return (
     <>
-      <ActionBar actions={actions} showSearchBar={showSearchBar} />
-      <SalesList searchFilter={filter} />
+      <ActionBar actions={actions} />
+      <SalesList />
     </>
   );
 };
