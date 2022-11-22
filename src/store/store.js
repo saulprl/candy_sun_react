b026e88d3ff6f621abf7675c85ef4d7738d62bdb
Saul <phasecross@hotmail.com>
2022-11-21 19:35:57 -0700

    Added the cart reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import uiReducer from "./uiSlice";
+import cartReducer from "./cartSlice";
 
 export default configureStore({
   reducer: {
     ui: uiReducer,
+    cart: cartReducer,
   },
 });
