dca5f99cdcc84eac11d04e738b77f385efabf09d
Saul <phasecross@hotmail.com>
2022-11-22 18:27:20 -0700

    Added render props to all three routes

--- src/pages/filters/FiltersPage.js
+++ src/pages/filters/FiltersPage.js
@@ -1,11 +1,19 @@
 import { Route } from "react-router-dom";
 
+import EmployeesFilters from "../../components/filters/EmployeeFilters";
+import ProductFilters from "../../components/filters/ProductFilters";
+import SaleFilters from "../../components/filters/SaleFilters";
+
 const FiltersPage = () => {
   return (
     <>
-      <Route path="/filters/employees" exact />
-      <Route path="/filters/products" exact />
-      <Route path="/filters/sales" exact />
+      <Route
+        path="/filters/employees"
+        exact
+        render={() => <EmployeesFilters />}
+      />
+      <Route path="/filters/products" exact render={() => <ProductFilters />} />
+      <Route path="/filters/sales" exact render={() => <SaleFilters />} />
     </>
   );
 };
