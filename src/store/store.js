02435e927fdda28029afc8622de750464b4d97ba
Saul <phasecross@hotmail.com>
2022-11-22 18:27:58 -0700

    Added filtersSlice as a reducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import uiReducer from "./uiSlice";
 import cartReducer from "./cartSlice";
+import filtersReducer from "./filtersSlice";
 
 export default configureStore({
   reducer: {
     ui: uiReducer,
     cart: cartReducer,
+    filters: filtersReducer,
   },
 });
